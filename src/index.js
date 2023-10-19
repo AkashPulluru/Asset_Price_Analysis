53ac424033bb5a1e13047ee0b827706a186282a7
Akash Pulluru <akashpulluru@gmail.com>
2023-10-19 14:33:32 -0400

    waiting to be un rate limited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import Stocks from './stocks';
 import View from './view';
+import Calculator from './calculator'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const stock = new Stocks('META');
-    const view = new View(stock, 'stock1-container'); 
-    view.createData();
+document.addEventListener('DOMContentLoaded', async () => {
+    const stock1 = new Stocks('META');
+    const view1 = new View(stock1, 'stock1-container'); 
+    await view1.createData();
 
     const stock2 = new Stocks('IBM');
     const view2 = new View(stock2, 'stock2-container'); 
-    view2.createData();
+    await view2.createData();
+
+    // const calculator = new Calculator(stock1, stock2);
+    // const correlation = await calculator.calculateCorrelations();
+    // console.log(`Correlation between META and IBM is: ${correlation}`);
 });
--- src/stocks.js
+++ src/stocks.js
@@ -16,20 +16,18 @@ class Stocks {
             console.error('Error fetching data:', error);
         }
     }
-}
 
+    async getPrices() {
+        this.storedData = await this.getData();
+        if (!this.storedData) return;
+        
+        let closingPrices = [];
+        for (let date in this.storedData["Monthly Time Series"]) {
+            closingPrices.push(parseFloat(this.storedData["Monthly Time Series"][date]['4. close']));
+        }
+        return closingPrices;
+    }
+    }
 export default Stocks;
 
 
-    // async getPrices() {
-    //     this.storedData = await this.getData();
-    //     if (!this.storedData) return;
-
-        
-    //     let closingPrices = {};
-
-    //     for (let date in this.storedData["Monthly Time Series"]) {
-    //         closingPrices[date] = this.storedData["Monthly Time Series"][date]['4. close'];
-    //     }
-    //     console.log(closingPrices);
-    // }
